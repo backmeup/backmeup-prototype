c447033acfd3a02e23bbf16f357f4096365f724b
Rainer Simon <rainer.simon@ait.ac.at>
2012-05-12 10:23:59 +0100

    Action base interface

--- org.backmeup.plugin.api/src/main/java/org/backmeup/plugin/api/actions/Action.java
+++ org.backmeup.plugin.api/src/main/java/org/backmeup/plugin/api/actions/Action.java
@@ -1,5 +1,14 @@
 package org.backmeup.plugin.api.actions;
 
-public class Action {
+import java.util.Properties;
 
+import org.backmeup.plugin.api.connectors.Progressable;
+import org.backmeup.plugin.api.storage.StorageException;
+import org.backmeup.plugin.api.storage.StorageReader;
+
+public interface Action {
+
+	public String doAction(Properties accessData, StorageReader storage, Progressable progressor)
+			throws ActionException;
+	
 }

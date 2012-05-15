417dfea49b6e2b5ea9e354970fcdd09679d2cfc0
Rainer Simon <rainer.simon@ait.ac.at>
2012-05-15 10:57:19 +0200

    Transferred indexing code from proof-of-concept project

--- org.backmeup.plugin.api/src/main/java/org/backmeup/plugin/api/actions/Action.java
+++ org.backmeup.plugin.api/src/main/java/org/backmeup/plugin/api/actions/Action.java
@@ -3,7 +3,6 @@ package org.backmeup.plugin.api.actions;
 import java.util.Properties;
 
 import org.backmeup.plugin.api.connectors.Progressable;
-import org.backmeup.plugin.api.storage.StorageException;
 import org.backmeup.plugin.api.storage.StorageReader;
 
 public interface Action {

29724bc34586683774487a8c8cb6c21f1e1401ae
Rainer Simon <rainer.simon@ait.ac.at>
2012-08-16 13:09:47 +0200

    Submission of dummy map/reduce job onto embedded cluster

---
+++ org.backmeup.job.impl/src/main/java/org/backmeup/job/impl/hadoop/BackupJobRunner.java
@@ -0,0 +1,37 @@
+package org.backmeup.job.impl.hadoop;
+
+import java.io.IOException;
+
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapRunnable;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.RecordReader;
+import org.apache.hadoop.mapred.Reporter;
+
+public class BackupJobRunner implements MapRunnable {
+
+	private JobConf conf;
+	
+	@Override
+	public void configure(JobConf conf) {
+		this.conf = conf;
+	}
+
+	@Override
+	public void run(RecordReader reader, OutputCollector outputCollector, Reporter reporter)
+			throws IOException {
+		
+		System.out.println("Starting backup job");
+		
+		/*
+		Text key = reader.createKey();
+		BytesWritable value = reader.createValue();
+		while (reader.next(key, value)) {
+			System.out.println("##### " + key + ": " + value);
+		}
+		*/
+		
+		System.out.println("Backupjob complete.");
+	}
+
+}
--- org.backmeup.job.impl/src/test/java/org/backmeup/job/impl/HadoopJobManagerTest.java
+++ org.backmeup.job.impl/src/test/java/org/backmeup/job/impl/HadoopJobManagerTest.java
@@ -1,11 +1,21 @@
 package org.backmeup.job.impl;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.MiniMRCluster;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+import org.backmeup.job.impl.hadoop.BackupJobRunner;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,12 +23,14 @@ import org.junit.Test;
 public class HadoopJobManagerTest {
 
 	private static final String TEST_LOG_DIR = "hadoop-logs";
+	private static final String TEST_INPUT_PATH = "src/test/resources";
+	private static final String TEST_OUTPUT_PATH = "hadoop-output";
 
 	private MiniDFSCluster dfsCluster = null;
 	private MiniMRCluster mrCluster = null;
 	
 	private final Path input = new Path("input");
-	private final Path output = new Path("output");
+	private final Path output = new Path(TEST_OUTPUT_PATH);
 	
 	@Before
 	public void setUp() throws Exception {
@@ -37,13 +49,45 @@ public class HadoopJobManagerTest {
 	    dfsCluster.getFileSystem().makeQualified(output);
 	    
 	    mrCluster = new MiniMRCluster(1, dfsCluster.getFileSystem().getUri().toString(), 1);
-	    System.out.println("Done.");
+	    transferFiles();
+	}	
+	
+	private void transferFiles() throws IOException {
+		System.out.println("Copying files to storage cluster");
+		
+		// Wouldn't a Scala .map one-liner be nice here...
+		// http://topsy.com/twitter.com/natpryce/status/223443583557042176
+		List<Path> srcFiles = new ArrayList<Path>(); 
+		for (File f : new File(TEST_INPUT_PATH).listFiles()) {
+			srcFiles.add(new Path(f.getAbsolutePath()));
+		}
+		
+		FileSystem fs = dfsCluster.getFileSystem();
+		fs.copyFromLocalFile(false, true, srcFiles.toArray(new Path[srcFiles.size()]), input);
+		System.out.println("Done.");
+	}
+	
+	private JobConf createJobConf() {
+		JobConf jobConf = mrCluster.createJobConf();
+		jobConf.setJobName("unitTest");
+		
+		jobConf.setJarByClass(BackupJobRunner.class);
+		jobConf.setMapRunnerClass(BackupJobRunner.class);	
+		jobConf.setInputFormat(TextInputFormat.class);
+		TextInputFormat.setInputPaths(jobConf, input);
+		
+		// We just set the output path to make hadoop happy.
+		TextOutputFormat.setOutputPath(jobConf, output);
+		
+		jobConf.setSpeculativeExecution(false);
+		return jobConf;
 	}
 	
 	@Test
 	public void testJobExecution() throws Exception {
-		// TODO 
-		System.out.println("testing...");
+		System.out.println("Scheduling job now.");
+		
+	    JobClient.runJob(createJobConf());
 	}
 	
 	@After
@@ -57,6 +101,9 @@ public class HadoopJobManagerTest {
 	    	mrCluster.shutdown();
 	    	mrCluster = null;
 	    }
+	    
+	    FileUtils.deleteDirectory(new File(TEST_LOG_DIR));
+	    FileUtils.deleteDirectory(new File(TEST_OUTPUT_PATH));
 	}
 	
 }

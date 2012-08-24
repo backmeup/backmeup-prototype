37eebf6acc84e1fc180230df7b4a10cade3b1c95
Rainer Simon <rainer.simon@ait.ac.at>
2012-08-24 10:27:22 +0200

    RabbitMQ receiver

--- org.backmeup.job.impl/src/main/java/org/backmeup/job/impl/rabbitmq/RabbitMQJobReceiver.java
+++ org.backmeup.job.impl/src/main/java/org/backmeup/job/impl/rabbitmq/RabbitMQJobReceiver.java
@@ -1,13 +1,19 @@
 package org.backmeup.job.impl.rabbitmq;
 
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
 import org.backmeup.plugin.Plugin;
 import org.backmeup.plugin.osgi.PluginImpl;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
 
 /**
- * Experimental. An alternative to Hadoop-based clustering, based on a simple
- * standalone Java app, picking up jobs via RabbitMQ.
+ * A Receiver class that listens to a RabbitMQ message queue and executes Backup jobs that
+ * get sent across the wire.
  * 
  * @author Rainer Simon <rainer.simon@ait.ac.at>
  */
@@ -24,31 +30,86 @@ public class RabbitMQJobReceiver {
 	
 	private Plugin plugins;
 	
-	public static void main(String[] args) {
-		System.out.println("Starting...");
-	}
+	/**
+	 * Message queue name
+	 */
+	private String mqName;
 	
-	public RabbitMQJobReceiver(String pluginsDir, String osgiTempDir) {
+	/**
+	 * Message queue connection
+	 */
+	private Connection mqConnection;
+	
+	/**
+	 * Message queue channel
+	 */
+	private Channel mqChannel;
+	
+	/**
+	 * Flag to switch message listening on and off
+	 */
+	private boolean listening = false;
+	
+	private Logger log = Logger.getLogger(this.getClass());
+	
+	public RabbitMQJobReceiver(String mqHost, String mqName, String pluginsDir, String osgiTempDir) throws IOException {
+		this.mqName = mqName;
+		
 		// Start up the Plugin manager
 		this.plugins = new PluginImpl(pluginsDir, osgiTempDir, EXPORTED_PACKAGES);
 		this.plugins.startup();
 	    ((PluginImpl)plugins).waitForInitialStartup();
+	    
+	    // Connect to the message queue
+		ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost(mqHost);
+	    
+	    mqConnection = factory.newConnection();
+	    mqChannel = mqConnection.createChannel();
+	    mqChannel.queueDeclare(mqName, false, false, false, null);
 	}
 	
-	public class BackupJobWorker implements Runnable {
-		
-		public BackupJobWorker() {
-			ConnectionFactory factory = new ConnectionFactory();
+	public boolean isListening() {
+		return listening;
+	}
+	
+	public void start() {
+		if (!listening) {
+			listening = true;
 			
+			Thread t = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+					    QueueingConsumer consumer = new QueueingConsumer(mqChannel);
+					    mqChannel.basicConsume(mqName, true, consumer);
+						
+					    while (listening) {
+					    	QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+					    	String message = new String(delivery.getBody());
+					    	log.info("Received: " + message);
+					    }
+					    
+					    log.info("Stopping message queue receiver");
+					    mqChannel.close();
+					    mqConnection.close();
+					    log.info("Message queue receiver stopped");
+					} catch (IOException e) {
+						// Should only happen if message queue is down
+						log.fatal(e.getMessage() + " - message queue down?");
+						throw new RuntimeException(e);
+					} catch (InterruptedException e) {
+						throw new RuntimeException(e);						
+					}
+				}
+			});
+			
+			t.start();
 		}
-
-		@Override
-		public void run() {
-			System.out.println("Querying the job queue");
-			
-			
-		}
-		
+	}
+	
+	public void stop() {
+		listening = false;
 	}
 
 }

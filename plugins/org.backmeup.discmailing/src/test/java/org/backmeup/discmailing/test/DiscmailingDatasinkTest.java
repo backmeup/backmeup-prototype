d369d170691ed1b0dcc2e302750a1de0ff0cca75
tg-x-net-at <tg@x-net.at>
2012-08-30 13:05:00 +0200

    Method to generate a xml ticket for burning station

--- plugins/org.backmeup.discmailing/src/test/java/org/backmeup/discmailing/test/DiscmailingDatasinkTest.java
+++ plugins/org.backmeup.discmailing/src/test/java/org/backmeup/discmailing/test/DiscmailingDatasinkTest.java
@@ -23,19 +23,18 @@ public class DiscmailingDatasinkTest {
 	/**
 	 * @param args
 	 */
-	public static void main(String args[]) throws FileNotFoundException, IOException, StorageException {
+	/*public static void main(String args[]) throws FileNotFoundException, IOException, StorageException {
 		System.out.println("DiscDatasinkTest");
 		Properties props = new Properties();
 	    props.load(new FileInputStream(new File("/tmp/auth.props")));    
-			
-		DiscmailingDatasink sink = new DiscmailingDatasink();
+
+	    DiscmailingDatasink sink = new DiscmailingDatasink();
 		StorageReader sr = new LocalFilesystemStorageReader();
 		sr.open("/data/backmeup/users/1/");
-			
+
 		sink.upload(props, sr, new Progressable(){
 			@Override
 			public void progress(String message) {}
 		});	
-	}
-	
+	}*/	
 }

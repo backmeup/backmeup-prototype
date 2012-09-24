219cd12ee2ceb4d1dfcd02ad978a9a9ed516c7ef
fschoeppl <fabian.schoeppl@students.fh-hagenberg.at>
2012-09-24 09:33:32 +0200

    Added python tests for REST mail api

---
+++ python-tests/MailTests.py
@@ -0,0 +1,38 @@
+# -*- coding: ISO-8859-1 -*-
+
+from RESTBackMeUp import *
+from unittest import TestCase, skip
+import httplib
+
+HTML_TEST_MAIL = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+    "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>title</title>
+  </head>
+  <body>
+    <div style="
+        color: red;
+            font-family: cursive;
+            ">Hello World!</div>
+  </body>
+</html>
+"""
+
+class TestMails(TestCase):
+  def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+
+  def test_send_text_mail(self):
+    res = send_text_mail("backmeup71@gmx.at", "Test-Text", "This is a very simple text message")
+    self.assertEquals(res.code, httplib.NO_CONTENT)
+    
+  def test_send_html_mail(self):
+    res = send_html_mail("backmeup71@gmx.at", "Test-Html", HTML_TEST_MAIL)
+    self.assertEquals(res.code, httplib.NO_CONTENT)
+  

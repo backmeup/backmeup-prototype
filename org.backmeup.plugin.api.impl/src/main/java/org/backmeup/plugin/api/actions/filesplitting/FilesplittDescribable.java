fcb4040d7496c4c15a87c6a3271fc59686db4c63
root <root@cirus.(none)>
2012-09-25 12:00:37 +0200

    added options to filesplitt plugin

--- org.backmeup.plugin.api.impl/src/main/java/org/backmeup/plugin/api/actions/filesplitting/FilesplittDescribable.java
+++ org.backmeup.plugin.api.impl/src/main/java/org/backmeup/plugin/api/actions/filesplitting/FilesplittDescribable.java
@@ -1,9 +1,23 @@
 package org.backmeup.plugin.api.actions.filesplitting;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.backmeup.plugin.api.actions.BaseActionDescribable;
 //TODO move in own project
 public class FilesplittDescribable extends BaseActionDescribable { 
   public FilesplittDescribable() {
     super("filesplitt.properties");
+  }
+  
+  @Override
+  public List<String> getAvailableOptions ()
+  {
+	  List<String> options = new LinkedList<String> ();
+	  
+	  options.add ("Test Option 1");
+	  options.add ("Test Option 2");
+	  
+	  return options;
   }
 }

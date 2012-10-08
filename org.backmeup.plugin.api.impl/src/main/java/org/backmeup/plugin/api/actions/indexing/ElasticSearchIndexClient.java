fbcef1974bb33c898d87e904ff8c7a4fdb80cffe
Rainer Simon <rainer.simon@ait.ac.at>
2012-10-08 17:17:44 +0200

    ElasticSearch: update to latest version

--- org.backmeup.plugin.api.impl/src/main/java/org/backmeup/plugin/api/actions/indexing/ElasticSearchIndexClient.java
+++ org.backmeup.plugin.api.impl/src/main/java/org/backmeup/plugin/api/actions/indexing/ElasticSearchIndexClient.java
@@ -25,9 +25,8 @@ public class ElasticSearchIndexClient {
 	}
 	
 	public SearchResponse queryBackup(String username, String query) {
-		System.out.println("username: " + username);
 		QueryBuilder qBuilder = QueryBuilders.boolQuery()
-				// .must(QueryBuilders.matchQuery(FIELD_OWNER_NAME, username))
+				.must(QueryBuilders.matchQuery(FIELD_OWNER_NAME, username))
 				.must(QueryBuilders.queryString(query));
 		
 		return client.prepareSearch(INDEX_NAME).setQuery(qBuilder).execute().actionGet();

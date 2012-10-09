6e47784ca2b0c144b9d3fa58d253b9216f730ff6
fschoeppl <fabian.schoeppl@students.fh-hagenberg.at>
2012-10-09 10:59:06 +0200

    Removed unwanted output of json requests to the keyserver

--- org.backmeup.keyserver.client/src/main/java/org/backmeup/keyserver/client/impl/Keyserver.java
+++ org.backmeup.keyserver.client/src/main/java/org/backmeup/keyserver/client/impl/Keyserver.java
@@ -397,7 +397,7 @@ public class Keyserver implements org.backmeup.keyserver.client.Keyserver {
     }
     String json = g.toJson(new TokenRequest(userId, userPwd, services,
         authinfos, backupdate, reusable));
-    System.out.println("REQUESTING: " + json);
+    //System.out.println("REQUESTING: " + json);
     Result r = execute(path + "/tokens/token", ReqType.POST, json);
     if (r.response.getStatusLine().getStatusCode() == 200) {
       return g.fromJson(r.content, Token.class);
@@ -410,7 +410,7 @@ public class Keyserver implements org.backmeup.keyserver.client.Keyserver {
     Gson g = new Gson();
     String json = g.toJson(token);
     Result r = execute(path + "/tokens/data", ReqType.POST, json);
-    System.out.println("REQUESTING: " + json);
+    //System.out.println("REQUESTING: " + json);
     if (r.response.getStatusLine().getStatusCode() == 200) {
       return g.fromJson(r.content, AuthDataResult.class);
     }

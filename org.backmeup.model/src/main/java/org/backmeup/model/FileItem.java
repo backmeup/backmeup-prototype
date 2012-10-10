add61e180dafc9153dc12135798099f769842628
Rainer Simon <rainer.simon@ait.ac.at>
2012-10-10 11:55:45 +0200

    FileItem no longer a DB entity

--- org.backmeup.model/src/main/java/org/backmeup/model/FileItem.java
+++ org.backmeup.model/src/main/java/org/backmeup/model/FileItem.java
@@ -2,23 +2,9 @@ package org.backmeup.model;
 
 import java.util.Date;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-
-@Entity
 public class FileItem {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(nullable = false)
   private Long fileId;
 
-  @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
   private Status status;
 
   private String thumbnailURL;

8785074513844c4363040ed286d8ab477867b823
Rainer Simon <rainer.simon@ait.ac.at>
2012-10-17 11:46:09 +0200

    'Structured' status messages

---
+++ org.backmeup.model/src/main/java/org/backmeup/model/StatusCategory.java
@@ -0,0 +1,11 @@
+package org.backmeup.model;
+
+public class StatusCategory {
+	
+	public static final String INFO = "info";
+	
+	public static final String WARNING = "warning";
+	
+	public static final String ERROR = "error";
+
+}

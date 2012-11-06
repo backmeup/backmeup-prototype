41f39387206bcb9fe096444ae0ec72b3c0d93bcb
fschoeppl <fabian.schoeppl@students.fh-hagenberg.at>
2012-11-06 13:59:56 +0100

    DeleteUsers from keyserver script simplified

--- python-tests/keyserver/DeleteUsers.py
+++ python-tests/keyserver/DeleteUsers.py
@@ -1,19 +1,7 @@
 from REST import Comm
-from unittest import TestCase
-from time import mktime
-from datetime import datetime
-import httplib
 
-class DeleteUsers(TestCase):
-  def setUp(self):
-    self.tearDown()
-
-
-  def tearDown(self):
-    com = Comm()
-    for i in range(0, 500):
-      com.request("DELETE", "/users/" + str(i))
-
-  def test_delete_users(self):
-    pass
-
+com = Comm()
+for i in range(0, 500):
+  print i
+  com.request("DELETE", "/users/" + str(i))
+com.close()

13dec5c15650c862d2b7db5165d076a0b18ab630
Rainer Simon <rainer.simon@ait.ac.at>
2012-11-08 12:45:21 +0100

    Cleanup

--- org.backmeup.rest/src/main/java/org/backmeup/rest/Thumbnails.java
+++ org.backmeup.rest/src/main/java/org/backmeup/rest/Thumbnails.java
@@ -5,7 +5,6 @@ import java.io.File;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
@@ -15,21 +14,18 @@ public class Thumbnails extends Base {
 
 	@GET
 	@Path("/")
-	@Produces("application/json")
 	public Response foo() {
 		return Response.ok("foo").build();
 	}
 	
 	@GET
 	@Path("/{username}")
-	@Produces("application/json")
 	public Response bar(@PathParam("username") String username) {
 		return Response.ok("bar " + username).build();
 	}
 	
 	@GET
 	@Path("{username}/{fileId}")
-	@Produces("application/json")
 	public Response getThumbnail(@PathParam("username") String username, @PathParam("fileId") String fileId) {
 		System.out.println("Getting thumbnail for: " + username + ", " + fileId);
 		File f = getLogic().getThumbnail(username, fileId);

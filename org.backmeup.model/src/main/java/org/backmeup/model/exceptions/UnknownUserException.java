31efc5296525634dbdc656f8734f8e11f35793d5
fschoeppl <fabian.schoeppl@students.fh-hagenberg.at>
2012-11-12 11:37:54 +0100

    #74: UnknownUserException and UserNotActivatedException now use static messages

--- org.backmeup.model/src/main/java/org/backmeup/model/exceptions/UnknownUserException.java
+++ org.backmeup.model/src/main/java/org/backmeup/model/exceptions/UnknownUserException.java
@@ -10,8 +10,14 @@ package org.backmeup.model.exceptions;
  */
 public class UnknownUserException extends BackMeUpException {
 	private static final long serialVersionUID = 1L;
+  private String username;
 
 	public UnknownUserException(String username) {
-		super("Unknown user " + username);
+		super("Unknown user");
+		this.username = username;
+	}
+	
+	public String getUsername() {
+	  return this.username;
 	}
 }
--- org.backmeup.rest/src/main/java/org/backmeup/rest/exceptionmapper/UnknownUserExceptionMapper.java
+++ org.backmeup.rest/src/main/java/org/backmeup/rest/exceptionmapper/UnknownUserExceptionMapper.java
@@ -12,7 +12,19 @@ public class UnknownUserExceptionMapper implements
 		ExceptionMapper<UnknownUserException> {
 	
 	public Response toResponse(UnknownUserException uue) {
-		return Response.status(Response.Status.NOT_FOUND).entity(new ErrorEntity(UnknownUserException.class.getName(), uue.getMessage())).build();
+		return Response.status(Response.Status.NOT_FOUND).entity(new UnknownUserEntity(uue)).build();
 	}
+	
+	public static class UnknownUserEntity extends ErrorEntity {
+    private String username;
 
+    public UnknownUserEntity(UnknownUserException eve) {
+      super(eve.getClass().getName(), eve);
+      this.username = eve.getUsername();
+    }
+
+    public String getUsername() {
+      return username;
+    }
+  }
 }

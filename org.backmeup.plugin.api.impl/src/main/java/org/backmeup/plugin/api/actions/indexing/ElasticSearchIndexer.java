0c0969c1c779198a7f8074b178d96fe555c9abb5
Rainer Simon <rainer.simon@ait.ac.at>
2012-12-21 12:21:39 +0100

    Source/Sink 'profile names' instead of identification (which seems to be null for Plugins?!?)

--- org.backmeup.plugin.api.impl/src/main/java/org/backmeup/plugin/api/actions/indexing/ElasticSearchIndexer.java
+++ org.backmeup.plugin.api.impl/src/main/java/org/backmeup/plugin/api/actions/indexing/ElasticSearchIndexer.java
@@ -74,7 +74,7 @@ public class ElasticSearchIndexer {
 		contentBuilder.field(IndexUtils.FIELD_FILENAME, getFilename(dataObject.getPath()));
 		contentBuilder.field(IndexUtils.FIELD_PATH, dataObject.getPath());
 		contentBuilder.field(IndexUtils.FIELD_FILE_HASH, dataObject.getMD5Hash());
-		contentBuilder.field(IndexUtils.FIELD_BACKUP_SINK, job.getSinkProfile().getIdentification());
+		contentBuilder.field(IndexUtils.FIELD_BACKUP_SINK, job.getSinkProfile().getProfileName());
 		contentBuilder.field(IndexUtils.FIELD_BACKUP_AT, new Date().getTime());
 		contentBuilder.field(IndexUtils.FIELD_JOB_ID, job.getId());
 		contentBuilder.field(IndexUtils.FIELD_JOB_NAME, job.getJobTitle());
@@ -82,7 +82,7 @@ public class ElasticSearchIndexer {
 		// Where's my Scala .map and mkString!?!
 		List<String> sourceNames = new ArrayList<String>(); 
 		for (ProfileOptions source : job.getSourceProfiles()) {
-			sourceNames.add(source.getProfile().getIdentification());			
+			sourceNames.add(source.getProfile().getProfileName());			
 		}
 		contentBuilder.field(IndexUtils.FIELD_BACKUP_SOURCES, StringUtils.join(sourceNames, ", "));
 		

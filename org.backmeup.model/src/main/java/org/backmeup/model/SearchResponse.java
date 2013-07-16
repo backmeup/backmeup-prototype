ea8a09d1f17af52b8ad2cfa2956dc13a9fe027c7
root <root@cirus.(none)>
2013-07-16 17:28:43 +0200

    added debug infos

--- org.backmeup.model/src/main/java/org/backmeup/model/SearchResponse.java
+++ org.backmeup.model/src/main/java/org/backmeup/model/SearchResponse.java
@@ -118,7 +118,7 @@ public class SearchResponse {
 	}
 
 	public void setByJob(List<CountedEntry> byJob) {
-		System.out.println ("Added job to Searc Response");
+		System.out.println ("Added job to Searc Response. Count: " + byJob.size ());
 		this.byJob = byJob;
 	}
 
